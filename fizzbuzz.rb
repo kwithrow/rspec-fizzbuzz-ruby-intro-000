15ac9df394f9fb08d126b43ad57005b6bb157479
bold-class-9124 <kirstiewithrow@gmail.com>
2021-02-24 03:38:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,20 +12,6 @@ def fizzbuzz(int)
   end
 
 end
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-   return "FizzBuzz"
-  end
-
-  if int % 5 == 0
-   return "Buzz"
-  end
-
-  if int % 3 == 0
-   return "Fizz"
-  end
-
-end
 
 fizzbuzz(3)
 fizzbuzz(5)
